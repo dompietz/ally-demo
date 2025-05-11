75c019964a6bf75dec7c66446f17acedcf5b05c3
“dompietz” <dominikpietzko@gmail.com>
2025-05-11 16:04:42 +0200

    Fix TS build on Netlify: remove unused path import and add types

--- hcneu/vite.config.ts
+++ hcneu/vite.config.ts
@@ -1,18 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path-browserify'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      path: 'path-browserify',
-    },
-  },
   build: {
     rollupOptions: {
       input: {
-        main: 'index.html', // Avoid using __dirname
+        main: 'index.html',
       },
     },
   },
