cec1689a687a86b1e714655a00c737d715e5821f
“dompietz” <dominikpietzko@gmail.com>
2025-05-12 11:40:13 +0200

    fixed dependencies

--- hcneu/src/components/Header.tsx
+++ hcneu/src/components/Header.tsx
@@ -1,4 +1,3 @@
-// src/components/Header.tsx
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
@@ -6,7 +5,7 @@ import "./Header.css";
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
   const [scrolled, setScrolled] = useState(false);
-  const toggleMenu = () => setIsOpen(x => !x);
+  const toggleMenu = () => setIsOpen((x) => !x);
 
   useEffect(() => {
     const sentinel = document.getElementById("top-sentinel");
@@ -22,22 +21,28 @@ export default function Header() {
   }, []);
 
   return (
-    <header className={`header${scrolled ? " header--scrolled" : " header--transparent"}`}>
+    <header className={`header${scrolled ? " header--scrolled" : ""}`}>
       <div className="header__inner">
         <Link className="header__logo" to="/">
-          <img
-            src={
-              scrolled
-                ? "https://health-covery.com/wp-content/uploads/2025/05/ALLYlogonobg1311.png"
-                : "https://health-covery.com/wp-content/uploads/2025/05/ALLYlogo-white.png"
-            }
-            alt="Ally Logo"
-          />
+          <picture>
+            <source
+              srcSet="https://health-covery.com/wp-content/uploads/2025/05/ALLYlogonobg1311.png"
+              media="(max-width: 800px)"
+            />
+            <img
+              src={
+                scrolled
+                  ? "https://health-covery.com/wp-content/uploads/2025/05/ALLYlogonobg1311.png"
+                  : "https://health-covery.com/wp-content/uploads/2025/05/ALLYlogo-white.webp"
+              }
+              alt="Ally Logo"
+            />
+          </picture>
         </Link>
 
         <nav className="header__nav--desktop">
-          <a href="#about" className={!scrolled ? "white-text" : ""}>Über Ally</a>
-          <a href="#faq" className={!scrolled ? "white-text" : ""}>FAQ</a>
+          <a href="#about">Über Ally</a>
+          <a href="#faq">FAQ</a>
           <Link to="/signup" className="nav-button filled">Sign Up</Link>
           <Link to="/login" className="nav-button outline">Log In</Link>
         </nav>
