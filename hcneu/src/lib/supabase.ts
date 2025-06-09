953718e86557bf151762345b9a2bc3306aa17ab3
dompietz <dominikpietzko@gmail.com>
2025-06-09 11:53:26 +0200

    Use env vars for Supabase config

--- hcneu/src/lib/supabase.ts
+++ hcneu/src/lib/supabase.ts
@@ -2,6 +2,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  'https://ryrohlvnchlltgdoiudd.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5cm9obHZuY2hsbHRnZG9pdWRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc1MjAzNzYsImV4cCI6MjA2MzA5NjM3Nn0.cwRc2DXwt4dQJ-XLy5Uld7l3dSU_QTSOZLaueIRYZQo'
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
 );
